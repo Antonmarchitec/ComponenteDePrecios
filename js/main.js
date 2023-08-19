19e4b99095cebac18b7a5dde9782463a1e6758fe
mi nombre <desarrollowebaps@gmail.com>
2023-08-18 22:40:18 -0400

    07_componentePrecios

--- js/main.js
+++ js/main.js
@@ -27,37 +27,28 @@ const pintarElementos = (data) =>{
     data.forEach(element => {
         //console.log(element)
         base_template.querySelector("#title").textContent = element.state
-        base_template.querySelector("#number").textContent = element.score
-        base_template.querySelector("#storage").textContent = element.storage
-        base_template.querySelector("#users").textContent = element.user
-        base_template.querySelector("#send").textContent = element.send
-        base_template.querySelector("#buton").dataset.btn = element.btn
-
-        //styles bloque 2
-        base_template.querySelector("#block").style.background = element.color_base 
         base_template.querySelector("#title").style.color = element.color_text
+
+        base_template.querySelector("#number").textContent = element.score
         base_template.querySelector("#number").style.color = element.color_text 
+
+        base_template.querySelector("#storage").textContent = element.storage
         base_template.querySelector("#storage").style.color = element.color_text 
+
+        base_template.querySelector("#users").textContent = element.user
         base_template.querySelector("#users").style.color = element.color_text 
+
+        base_template.querySelector("#send").textContent = element.send
         base_template.querySelector("#send").style.color = element.color_text 
+
+        base_template.querySelector("#buton").dataset.btn = element.btn
         base_template.querySelector("#buton").style.background = element.color_btn 
-        base_template.querySelector("#buton").style.color = element.color_text_btn 
+        base_template.querySelector("#buton").style.color = element.color_text_btn   
 
+        base_template.querySelector("#block").style.background = element.color_base 
+        base_template.querySelector("#block").style.width = element.width 
+        base_template.querySelector("#block").style.height = element.height 
 
-
-
-
-           
-        
-        /*
-        base_template.querySelector("#number").style.color = element.colorText  
-        base_template.querySelector("#title").style.color = element.colorText
-        base_template.querySelector("#storage").style.color = element.colorText
-        base_template.querySelector("#users").style.color = element.colorText
-        base_template.querySelector("#send").style.color = element.colorText
-        base_template.querySelector("#buton").style.background = element.colorBackgrundWhite
-        base_template.querySelector("#buton").style.background = element.colorBackgrundWhite
-        */
 
 
         let clone = base_template.cloneNode(true)
